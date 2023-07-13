5980369dd0ce3516a6b28caaaf0a24494a11250c
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-07-13 12:47:06 +0530

    Update

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Username() {
-  return <div className="text-sm font-semibold">Jayesh</div>;
+  return <div className="hidden text-sm font-semibold md:block">Jayesh</div>;
 }
